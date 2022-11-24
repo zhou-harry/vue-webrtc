59ea2c7a5a5a27d178f18e2a795960708d5f6fc3
zhouhong <hong.zhou@eacloud.com>
2022-11-24 21:02:57 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <demo>
+    </demo>
+</template>
+
+<script>
+    import Demo from './components/Demo.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            'demo': Demo
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
